1830bbc4862a8ed2c368bc118e85150f2244efff
Ulisses Alves <ulisalves@gmail.com>
2022-08-22 16:53:43 -0300

    fiveth commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import CONST from './components/data/contants';
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
 import Carousel from './components/Carousel'
+import Footer from './components/Footer'
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -13,26 +14,43 @@ const App = () => {
   const { URL, APISTRING } = CONST;
 
   const [ movies, setMovies ] = useState<any>();
+  const [series, setSeries] = useState<any>();
   
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(`${URL}/discover/movie${APISTRING}&sort_by=popularity.desc`);
+      const movies = await fetch(`${URL}/discover/movie${APISTRING}&sort_by=popularity.desc`);
+      const moviesData = await movies.json();
+      setMovies(moviesData);
 
-      const data = await response.json();
-
-      setMovies(data);
+      const series = await fetch(`${URL}/discover/tv${APISTRING}&sort_by=popularity.desc`);
+      const seriesData = await series.json();
+      setSeries(seriesData);
     }
       fetchData();
     }, []);
 
+  // useEffect(() => (movies && series) && console.log(movies, series), [ movies, series ])
+
+  const getFeaturedMovie = () => movies && movies?.results[0];
+
+  const getMovieList = () => {
+    if (movies) {
+      const [featured, ...movieList] = movies?.results;
+      return movieList;
+    }
+    return [];
+  };
+
+  //const [featured, ...movieList] = movies?.results;
 
   return (
     <div className="m-auto antialised font-sans bg-black text-white">
-       <Hero {...movies?.results[0]}/>
+       <Hero {...getFeaturedMovie()}/>
       <NavBar />
-      <Carousel />
-      <Carousel />
-      <Carousel />
+      <Carousel title='Filmes Populares' data={getMovieList()} />
+      <Carousel title='Séries Populares' data={series?.results}/>
+      <Carousel title='placeholder' />
+      <Footer />
     </div>
   );
 }
--- src/components/data/mock.tsx
+++ src/components/data/mock.tsx
@@ -3,60 +3,62 @@ import cover2 from '../../assets/movie2.jpg';
 import cover3 from '../../assets/movie3.jpg';
 
 export interface Movie {
-  title: string;
-  cover: string;
-  score?: number | string;
+  title?: string;
+  name?: string;
+  cover?: string;
+  poster_path?: string;
+  vote_average?: number | string;
 }
 
 const mockData: Movie[] = [
     {
-        title: "Avengers",
-        cover: cover1,
-    },
-     {
-        title: "Missão Impossível",
-        cover: cover2,
-    },
-     {
-        title: "Mega Tubarão",
-        cover: cover3,
-    },
-    {
-        title: "Avengers",
-        cover: cover1,
-    },
-     {
-        title: "Missão Impossível",
-        cover: cover2,
-    },
-     {
-        title: "Mega Tubarão",
-        cover: cover3,
-    },
-    {
-        title: "Avengers",
-        cover: cover1,
-    },
-     {
-        title: "Missão Impossível",
-        cover: cover2,
-    },
-     {
-        title: "Mega Tubarão",
-        cover: cover3,
-    },
-    {
-        title: "Avengers",
-        cover: cover1,
-    },
-     {
-        title: "Missão Impossível",
-        cover: cover2,
-    },
-     {
-        title: "Mega Tubarão",
-        cover: cover3,
-    }
+    title: 'Avengers',
+    cover: cover1,
+  },
+  {
+    title: 'Missão Impossível',
+    cover: cover2,
+  },
+  {
+    title: 'Mega Tubarão',
+    cover: cover3,
+  },
+  {
+    title: 'Avengers',
+    cover: cover1,
+  },
+  {
+    title: 'Missão Impossível',
+    cover: cover2,
+  },
+  {
+    title: 'Mega Tubarão',
+    cover: cover3,
+  },
+  {
+    title: 'Avengers',
+    cover: cover1,
+  },
+  {
+    title: 'Missão Impossível',
+    cover: cover2,
+  },
+  {
+    title: 'Mega Tubarão',
+    cover: cover3,
+  },
+  {
+    title: 'Avengers',
+    cover: cover1,
+  },
+  {
+    title: 'Missão Impossível',
+    cover: cover2,
+  },
+  {
+    title: 'Mega Tubarão',
+    cover: cover3,
+  },
 ];
 
 export default mockData;
