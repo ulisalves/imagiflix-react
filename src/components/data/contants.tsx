94ea6eb9676fa0d62259e4124e11dbf9ebc76237
Ulisses Alves <ulisalves@gmail.com>
2022-08-22 08:30:27 -0300

    forth commit v2

---
+++ src/components/data/contants.tsx
@@ -0,0 +1,11 @@
+  const URL = 'https://api.themoviedb.org/3';
+  const IMAGEURL ='https://image.tmdb.org/t/p';
+  const APIKEY = '703044e6ebd5a3df77f906836fec13ec';
+  const APISTRING = `?api_key=${APIKEY}&language=pt-BR`;
+
+  export default {
+    URL,
+    IMAGEURL,
+    APIKEY,
+    APISTRING
+};
